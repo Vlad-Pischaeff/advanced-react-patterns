fe9e6e543f5be4af073d242ad801ad7f3871cd56
vlad-pischaeff <pischaeff@gmail.com>
2023-09-13 12:16:50 +0500

    minor fixes

--- src/patterns/compound-component-useState/components/Decrement.js
+++ src/patterns/compound-component-useState/components/Decrement.js
@@ -4,12 +4,12 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useCounterContext } from "../useCounterContext";
 
 function Decrement({ icon = "minus" }) {
-  const { handleDecrement } = useCounterContext();
-  return (
-    <StyledButton onClick={handleDecrement}>
-      <FontAwesomeIcon icon={icon} color="#17a2b8" />
-    </StyledButton>
-  );
+    const { handleDecrement } = useCounterContext();
+    return (
+        <StyledButton onClick={handleDecrement}>
+            <FontAwesomeIcon icon={icon} color="#17a2b8" />
+        </StyledButton>
+    );
 }
 
 export { Decrement };
